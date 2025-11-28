bd47d8b5d26a7c865358bccd716dc0dfca460415
Thaís Sutil <thaissutil.dev@gmail.com>
2025-11-27 21:41:57 -0300

    Adiciona variáveis de produto e preço.

---
+++ main.py
@@ -0,0 +1,10 @@
+# Projeto Análise de Vendas
+
+nome_produto = "Cadeira Gamer"
+preco_produto = 899.90
+quant_vendas = 7
+
+print("===== CUPOM FISCAL =====")
+print("Produto: ", nome_produto)
+print("Preço: ", preco_produto)
+print("Total Vendas: ", quant_vendas)
